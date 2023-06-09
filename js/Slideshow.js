862a6c6bd547a83350cf5612e3602fbeacaec3d0
Sébastien <sebast.senechal@gmail.com>
2023-06-09 18:43:41 +0200

    reindent

--- js/Slideshow.js
+++ js/Slideshow.js
@@ -1,5 +1,5 @@
 class Slideshow {
-
+    
     constructor(items, duration) {
         this.items = document.getElementsByClassName(items); // DOM element for slide
         this.diapoNum = 0;
@@ -8,65 +8,65 @@ class Slideshow {
         this.autoplay = this.autoPlay();
         this.keysKeyboard = document.addEventListener("keydown", this.keysKeyboard.bind(this));
     }
-
+    
     autoPlay() {
         this.timer = setInterval(this.next.bind(this), this.duration);
         // Millisecondes par tour, 5 secondes par défaut
         console.log('Autoplay : ' + this.playing);
     } 
-
+    
     next() {
         this.items[this.diapoNum].style.opacity = "0";
-		if (this.diapoNum === this.items.length -1) {
-			this.diapoNum = 0;
-		} else {
-			this.diapoNum++;
-		}
-		this.items[this.diapoNum].style.opacity = "1";
+        if (this.diapoNum === this.items.length -1) {
+            this.diapoNum = 0;
+        } else {
+            this.diapoNum++;
+        }
+        this.items[this.diapoNum].style.opacity = "1";
     }
-
+    
     previous() {
-		this.items[this.diapoNum].style.opacity = "0";
-		if (this.diapoNum === 0) {
-			this.diapoNum = this.items.length -1;
-		} else {
-			this.diapoNum--;
-		}
-		this.items[this.diapoNum].style.opacity = "1";
-	}
-
+        this.items[this.diapoNum].style.opacity = "0";
+        if (this.diapoNum === 0) {
+            this.diapoNum = this.items.length -1;
+        } else {
+            this.diapoNum--;
+        }
+        this.items[this.diapoNum].style.opacity = "1";
+    }
+    
     // Mise en pause et lecture
-	pauseSlideshow() {
-		this.playing = false;
-		clearInterval(this.timer);
+    pauseSlideshow() {
+        this.playing = false;
+        clearInterval(this.timer);
         console.log('Autoplay : ' + this.playing);
-	}
-
+    }
+    
     playSlideshow() {
-		this.playing = true;
-		this.autoPlay();
-	}
-
-	pauseButton() {
-		if (this.playing === true) {
-			document.getElementById("pauseSlideImg").src = pauseSlideImg.src.replace("pause-blue", "play-blue");
-			this.pauseSlideshow();
-		}
-		else {
-			document.getElementById("pauseSlideImg").src = pauseSlideImg.src.replace("play-blue", "pause-blue");
-			this.playSlideshow();
-		}
-	}
-
+        this.playing = true;
+        this.autoPlay();
+    }
+    
+    pauseButton() {
+        if (this.playing === true) {
+            document.getElementById("pauseSlideImg").src = pauseSlideImg.src.replace("pause-blue", "play-blue");
+            this.pauseSlideshow();
+        }
+        else {
+            document.getElementById("pauseSlideImg").src = pauseSlideImg.src.replace("play-blue", "pause-blue");
+            this.playSlideshow();
+        }
+    }
+    
     keysKeyboard(event) {
-		if (event.keyCode === 39) {
-        document.addEventListener("keydown", this.next()); // Appuyer sur suivant
-    } else if (event.keyCode === 37) {
-        document.addEventListener("keydown", this.previous()); // Appuyer sur précédent
-    } else if ((event.keyCode === 13) || (event.keyCode === 19) || (event.keyCode === 32)) {
-			document.addEventListener("keydown", this.pauseButton()); // Barre espace, touche Enter et Pause
-		}
-	}
+        if (event.keyCode === 39) {
+            document.addEventListener("keydown", this.next()); // Appuyer sur suivant
+        } else if (event.keyCode === 37) {
+            document.addEventListener("keydown", this.previous()); // Appuyer sur précédent
+        } else if ((event.keyCode === 13) || (event.keyCode === 19) || (event.keyCode === 32)) {
+            document.addEventListener("keydown", this.pauseButton()); // Barre espace, touche Enter et Pause
+        }
+    }
     
 }
 
